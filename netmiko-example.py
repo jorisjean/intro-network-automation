d9d65bbf6b2387e281f7544db6f512df4fbb4ee5
jorisjean <joris.jeannelle@gmail.com>
2020-07-24 19:30:47 +0200

    Update netmiko-example.py

--- netmiko-example.py
+++ netmiko-example.py
@@ -2,10 +2,10 @@ from netmiko import ConnectHandler
 
 csr1000v = {
     'device_type': 'cisco_ios',
-    'host':   '10.10.20.48',
+    'host':   'ios-xe-mgmt-latest.cisco.com',
     'username': 'developer',
     'password': 'C1sco12345',
-    'port': 22
+    'port': 8181
 }
 
 net_connect = ConnectHandler(**csr1000v)
@@ -13,10 +13,10 @@ net_connect = ConnectHandler(**csr1000v)
 output = net_connect.send_command('show version')
 print(output)
 
-# config_commands = [
-#     'interface lo100',
-#     'ip address 10.1.1.1 255.255.255.0',
-#     'description test-joris'
-# ]
-# output = net_connect.send_config_set(config_commands)
-# print(output)
+config_commands = [
+    'interface lo100',
+    'ip address 10.1.1.1 255.255.255.0',
+    'description test-joris'
+]
+output = net_connect.send_config_set(config_commands)
+print(output)
